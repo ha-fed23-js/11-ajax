8b1b2eabf356654e8ee3fe55584eb4fdbff67307
David Andersson <lejonmanen@gmail.com>
2023-12-06 10:14:39 +0100

    Chuck Norris API

--- script.js
+++ script.js
@@ -4,15 +4,29 @@ const pResults = document.querySelector('#fetch-results')
 // Använd console.log för att kontrollera innehållet i variabler
 // console.log('Fungerar DOM? ', button, pResults);
 
-button.addEventListener('click', () => {
+button.addEventListener('click', async () => {
 	// förbered request (URL, inställningar)
 	// skicka request
 	// vänta på svar
 	// omvandla svar till JS-objekt (via JSON)
 	// visa datan vi fick
 
-	const url = 'https://forverkliga.se/JavaScript/api/simple.php'
-	const response = fetch(url)
+	const url = 'https://api.chucknorris.io/jokes/random'
+	const response = await fetch(url)
 
 	console.log('Skickade request till URL, fick', response);
+	if( response.status === 200 ) {
+		console.log('Jippi, request lyckades');
+	} else {
+		console.log('Request misslyckades med felkod: ' + response.status);
+		return
+	}
+
+	const data = await response.json()
+	console.log('Data från servern', data);
+	pResults.innerText = data.value
 })
+
+
+// Quick suggestions heter listan som poppar upp medan man skriver
+// CORS = Cross-Origin Resource Sharing
