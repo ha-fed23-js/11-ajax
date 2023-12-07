27e805dcc5caed9d748def5b332fd308429804ca
David Andersson <lejonmanen@gmail.com>
2023-12-07 10:21:27 +0100

    History

--- advice/advice.js
+++ advice/advice.js
@@ -1,7 +1,11 @@
 const fortuneButton = document.querySelector('#fortune-button')
 const adviceContainer = document.querySelector('.advice')
+const historyContainer = document.querySelector('.advice-history')
 
 // console.log('DOM', fortuneButton, adviceContainer);
+
+
+
 
 fortuneButton.addEventListener('click', async () => {
 	// förbered request (URL, inställningar)
@@ -17,6 +21,14 @@ fortuneButton.addEventListener('click', async () => {
 		console.log('Data from API:', data);
 		// Datans format: " { slip: { advice, id } } "
 		adviceContainer.innerText = data.slip.advice
+
+		const element = document.createElement('li')
+		element.innerText = `${data.slip.id}: ${data.slip.advice}`
+
+		// historyContainer.append(element) - om man vill lägga till sist
+		const first = historyContainer.children[0]
+		// Alternativ med queryselector: .advice-history > :first-child
+		historyContainer.insertBefore(element, first)
 	}
 	catch(error) {
 		console.log('An error occurred! ' + error.message);
